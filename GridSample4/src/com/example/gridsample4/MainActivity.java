731fdf1dbd7ecdf39a9993464a1d760fe7daeeb0
A U Thor <in16ihodre@gmail.com>
2014-06-19 00:54:25 +0900

    ◎初期プッシュ
    6/19 0:54
    WINDOWS

---
+++ GridSample4/src/com/example/gridsample4/MainActivity.java
@@ -0,0 +1,221 @@
+package com.example.gridsample4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.widget.GridLayout;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener{
+
+	private  int right_button_id = 0;
+	private String right_name;
+	private int right_id;
+
+	int[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	private List<ImageButton> buttons;
+	private SQLiteDatabase db;
+	private AlphaAnimation feedout;
+	private int num_ok;
+	private int num_miss;
+	private int width;
+	private int x;
+	private int y;
+	private Bitmap bitmap;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		buttons = new ArrayList<ImageButton>();
+
+		MyOpenHelper helper = new MyOpenHelper(this);
+		db = helper.getReadableDatabase();
+
+		//正解画像透明
+		findViewById(R.id.ImageView1).setVisibility(ImageView.INVISIBLE);
+
+		feedout = new AlphaAnimation( 1, 0 );
+		feedout.setDuration(1000);
+
+		WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
+		// ディスプレイのインスタンス生成
+		Display disp = wm.getDefaultDisplay();
+		Point size = new Point();
+		disp.getSize(size);
+		width = size.x/3;
+	}
+	protected void onStart(){
+		super.onStart();
+		shuffle(list);
+
+		Cursor c = db.rawQuery("Select * from TableTest order by random() limit 1;", null);
+		c.moveToFirst();
+		Cursor c2 = db.rawQuery("Select * from TableTest where name <> ? order by random() limit 8;", new String[]{c.getString(1)});
+		c2.moveToFirst();
+
+		right_id = c.getInt(0);
+		right_name = c.getString(1);
+		num_ok = c.getInt(2);
+		num_miss = c.getInt(3);
+
+		Resources res = getResources();
+		for(int i=0;i<=c2.getCount();i++){
+			if(i==8){
+				//正解ボタン設定
+				right_button_id = 0x7f080001+list[i];
+				bitmap = BitmapFactory.decodeResource(res, 0x7f020001 + c.getInt(0));
+			}else{
+				//まわりのボタン設定
+				bitmap = BitmapFactory.decodeResource(res, 0x7f020001 + c2.getInt(0));
+				c2.moveToNext();
+			}
+			ImageButton Button = (ImageButton) findViewById(0x7f080001 + list[i]);
+			Button.setOnClickListener(this);
+			GridLayout.LayoutParams params1 = new GridLayout.LayoutParams();
+			params1.width = width;
+			params1.height = width;
+			ColumnRowNum(list[i]);
+			params1.columnSpec = GridLayout.spec(x);
+			params1.rowSpec = GridLayout.spec(y);
+
+			Bitmap bitmap2 = Bitmap.createScaledBitmap(bitmap, width-10, width-10, false);
+			Button.setImageBitmap(bitmap2);
+			Button.setLayoutParams(params1);
+			buttons.add(Button);
+		}
+
+		//ボタン有効化
+		allbuttonEnable(true);
+		TextView message = (TextView)this.findViewById(R.id.textView1);
+		message.setText(right_name + "  は？");
+
+		message = (TextView)this.findViewById(R.id.textView2);
+		message.setText("id:"+ right_id + "   "+"name:" + right_name);
+		//message.setText((char)buttons.size());
+
+		message = (TextView)this.findViewById(R.id.textView3);
+		message.setText(list[0] + "  "+  list[1] + "  " + list[2] + "  " + list[3] + "  " +list[4] + "  "  + list[5] + "  " + list[6] + "  " + list[7] + "  " + list[8]);
+
+		message = (TextView)this.findViewById(R.id.textView4);
+		//message.setText(2);
+	}
+
+	//ボタンが押されたら
+	@Override
+	public void onClick(View v) {
+		//ボタン無効化
+		allbuttonEnable(false);
+		ContentValues updateValues = new ContentValues();
+		//正解
+		if(v.getId() ==right_button_id ){
+			//Toast.makeText(MainActivity.this, "正解！", Toast.LENGTH_SHORT).show();
+			ImageView img = (ImageView)findViewById(R.id.ImageView1);
+
+			updateValues.put("ok",num_ok + 1 );
+
+			img.setImageResource(R.drawable.circle);
+			img.startAnimation( feedout );
+		}
+		//不正解
+		else{
+			updateValues.put("miss",num_miss + 1 );
+			for(int i = 0;i<8;i++){
+				ImageButton button = buttons.get(i);
+				button.startAnimation(feedout);
+			}
+		}
+		db.update("TableTest", updateValues, "name=?", new String[]{right_name});
+		buttons.clear();
+		//フェードアウト分の時間待ち
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				onStart();
+			}
+		}, 1000);
+	}
+
+	private void shuffle(int[] arr) {
+		for(int i=arr.length-1; i>0; i--){
+			int t = (int)(Math.random() * i);  //0～i-1の中から適当に選ぶ
+			//選ばれた値と交換する
+			int tmp = arr[i];
+			arr[i]  = arr[t];
+			arr[t]  = tmp;
+		}
+	}
+
+	private void allbuttonEnable(boolean b) {
+		for(int i = 0;i<buttons.size();i++){
+			ImageButton button = buttons.get(i);
+			button.setEnabled(b);
+		}
+	}
+
+	private void ColumnRowNum(int i) {
+		if(i==1){
+			x = 0;
+			y = 0;
+		}else if(i==2){
+			x = 1;
+			y = 0;
+		}else if(i==3){
+			x = 2;
+			y = 0;
+		}else if(i==4){
+			x = 0;
+			y = 1;
+		}else if(i==5){
+			x = 1;
+			y = 1;
+		}else if(i==6){
+			x = 2;
+			y = 1;
+		}else if(i==7){
+			x = 0;
+			y = 2;
+		}else if(i==8){
+			x = 1;
+			y = 2;
+		}else if(i==9){
+			x = 2;
+			y = 2;
+		}
+	}
+
+
+	/*protected void onResume(){
+		super.onResume();
+	}*/
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ GridSample4/src/com/example/gridsample4/MyOpenHelper.java
@@ -0,0 +1,90 @@
+package com.example.gridsample4;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+	 private static final String DB_FILE_NAME = "TableTest.db";
+	 private static final String DB_NAME = "TableTest";
+	 private static final int DB_VERSION = 1;
+
+	 private Context context;
+	 private File dbPath;
+	    private boolean createDatabase = false;
+
+	public MyOpenHelper(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+		this.context = context;
+		this.dbPath = context.getDatabasePath(DB_NAME);
+	}
+
+	@Override
+	public synchronized SQLiteDatabase getReadableDatabase() {
+		SQLiteDatabase database = super.getReadableDatabase();
+		if (createDatabase) {
+			try {
+				database = copyDatabase(database);
+			} catch (IOException e) {
+			}
+		}
+		return database;
+	}
+
+	@Override
+	public synchronized SQLiteDatabase getWritableDatabase() {
+		SQLiteDatabase database = super.getWritableDatabase();
+		if (createDatabase) {
+			try {
+				database = copyDatabase(database);
+			} catch (IOException e) {
+			}
+		}
+		return database;
+	}
+
+	private SQLiteDatabase copyDatabase(SQLiteDatabase database) throws IOException {
+		// dbがひらきっぱなしなので、書き換えできるように閉じる
+		database.close();
+
+		// コピー！
+		InputStream input = context.getAssets().open(DB_FILE_NAME);
+		OutputStream output = new FileOutputStream(this.dbPath);
+		copy(input, output);
+
+		createDatabase = false;
+		// dbを閉じたので、また開く
+		return super.getWritableDatabase();
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		super.onOpen(db);
+		// getWritableDatabase()したときに呼ばれてくるので、
+		// 初期化する必要があることを保存する
+		this.createDatabase = true;
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onUpgrade(db, oldVersion, newVersion);
+	}
+
+	// CopyUtilsからのコピペ
+	private static int copy(InputStream input, OutputStream output) throws IOException {
+		byte[] buffer = new byte[1024 * 4];
+		int count = 0;
+		int n = 0;
+		while (-1 != (n = input.read(buffer))) {
+			output.write(buffer, 0, n);
+			count += n;
+		}
+		return count;
+	}
+}
